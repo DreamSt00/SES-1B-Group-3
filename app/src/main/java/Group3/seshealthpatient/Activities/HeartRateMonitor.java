cd90c375055534d53fcd607141d2a4b98bb00dea
ASustiyono <12889337@student.uts.edu.au>
2018-08-25 22:44:17 +1000

    HeartRateMonitor Java class created and started coding feature - Alfons

---
+++ app/src/main/java/Group3/seshealthpatient/Activities/HeartRateMonitor.java
@@ -0,0 +1,66 @@
+package Group3.seshealthpatient.Activities;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+//import com.example.user.heart.R;
+
+
+
+/**
+ * Created by Alfons on 25/08/2018.
+ */
+
+public class HeartRateMonitor  extends Activity {
+
+    private static final String TAG = "HeartRateMonitor";
+    private static final AtomicBoolean processing = new AtomicBoolean(false);
+
+    private static SurfaceView preview = null;
+    private static SurfaceHolder previewHolder = null;
+    private static Camera camera = null;
+    private static TextView text = null;
+    private static TextView beatsText = null;
+
+    private static int beatsAverage = 0;
+
+    private static WakeLock wakeLock = null;
+
+    private static int averageIndex = 0;
+    private static final int averageArraySize = 4;
+    private static final int[] averageArray = new int[averageArraySize];
+
+    public enum TYPE {
+        GREEN, RED
+    }
+
+    private static TYPE currentType = TYPE.GREEN;
+
+    private static int beatsIndex = 0;
+    private static final int beatsArraySize = 3;
+    private static final int[] beatsArray = new int[beatsArraySize];
+    private static double beats = 0;
+    private static long startTime = 0;
+
+    private static int cnt = 0;
+    private static int seconds;
+
+    private static boolean isFlashOn = false;
+
+}
